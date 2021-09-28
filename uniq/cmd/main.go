b8d474ae461fdf8669479d1c3ea8248322c00a59
k-t-l-h <laciedreamer@gmail.com>
2021-09-28 14:14:46 +0300

    add params

--- uniq/cmd/main.go
+++ uniq/cmd/main.go
@@ -3,49 +3,65 @@ package main
 import (
 	"bufio"
 	"flag"
+	"fmt"
 	"github.com/k-t-l-h/go_2021_1_hw1/uniq/internal"
 	"io"
-	"log"
 	"os"
 )
 
-var inputFlag string
-var outputFlag string
+var p internal.Params
 
-func flagInit(p internal.Params) {
+func flagInit() {
 	flag.BoolVar(&(p.Count), "c", false, "count the number of occurrences")
 	flag.BoolVar(&(p.Double), "d", false, "print only repeated lines")
 	flag.BoolVar(&(p.Uniq), "u", false, "print only unique lines")
 	flag.BoolVar(&(p.Case), "i", false, "case sensitivity")
-	flag.StringVar(&inputFlag, "in", "", "input to file")
-	flag.StringVar(&outputFlag, "out", "", "output to file")
+
+	flag.IntVar(&(p.FieldsOffset), "f", 0, "skip n fields")
+	flag.IntVar(&(p.CharOffset), "s", 0, "skip n chars")
 }
 
 func main() {
-	var p internal.Params
-	flagInit(p)
+	var (
+		inputFlag  string
+		outputFlag string
+		err        error
+	)
+	flagInit()
 	flag.Parse()
 
-	log.Print(p)
 	input := os.Stdin
 	output := os.Stdout
 
+	inputFlag = flag.Arg(0)
+	outputFlag = flag.Arg(1)
+
 	if inputFlag != "" {
-		input, _ = os.Open(inputFlag)
+		input, err = os.Open(inputFlag)
+		if err != nil {
+			fmt.Print("Не удалось открыть файл на чтение")
+			os.Exit(-1)
+		}
 	}
 
 	if outputFlag != "" {
-		output, _ = os.Open(outputFlag)
+		output, err = os.Create(outputFlag)
+		if err != nil {
+			fmt.Print("Не удалось открыть файл на запись")
+			os.Exit(-1)
+		}
 	}
 
-	if !checkFlags(true, true, false) {
+	if !checkFlags(p.Double, p.Uniq, p.Count) {
 		flag.Usage()
 	}
 
 	values := getLines(input)
 	values = internal.Uniq(values, p)
-	log.Print(values)
-	log.Print(giveLines(output, values))
+	err = giveLines(output, values)
+	if err != nil {
+		fmt.Print("Не удалось записать в файл")
+	}
 
 }
 
@@ -70,5 +86,5 @@ func giveLines(output io.Writer, table []string) error {
 }
 
 func checkFlags(a, b, c bool) bool {
-	return !(a&& b) && !(a&&c) && !(b&&c) && (!(a && b && c) || !(a || b || c))
+	return !((a && b) || (a && c) || (b && c))
 }
